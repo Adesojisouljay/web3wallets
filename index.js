ee2690f36b4652a4c52394e916475eb3369d6eb8
Adesojisouljay <adesojiisrael@gmail.com>
2026-01-23 06:44:34 +0100

    change port

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.use(express.json());
 app.use("/api", routes);
 // console.log("process.env.ETH_RPC_URLuuu;...",process.env.ETH_RPC_URL)
 
-app.listen(2000, () => console.log("Server running on port 2000"));
+app.listen(4001, () => console.log("Server running on port 4001"));
